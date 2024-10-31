a4f4f9c3752933cee2fd8c473ee270fd1dcac97b
crisis82 <filmerlo99@gmail.com>
2024-10-31 19:55:32 +0100

    added field for storing last operation timestamp

--- src/node.hrl
+++ src/node.hrl
@@ -1,3 +1,5 @@
 %% Records' definition for node and leader
 -record(node, {x, y, parent, children = [], time, leaderID, pid, neighbors = []}).
--record(leader, {node, color, serverID, adjClusters = [], nodes_in_cluster = []}).
+-record(leader, {
+    node, color, serverID, last_timestamp = 0, adjClusters = [], nodes_in_cluster = []
+}).
